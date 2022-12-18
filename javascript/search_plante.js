3646a204338b864486261c5f1ba81b7a238825ed
GrumpfGrincheux <valentin-petitcolin@orange.fr>
2022-12-18 21:28:45 +0100

    !

--- javascript/search_plante.js
+++ javascript/search_plante.js
@@ -1,90 +1,57 @@
 const planteForm = document.getElementById("planteForm");
-planteForm.addEventListener("input", function getInput() {
-	let send = new FormData(planteForm);
-	const xhr = new XMLHttpRequest();
-	xhr.open("POST", "/pages/plantes/php/search_plantes.php", true);
-	xhr.onload = () => {
-		if (xhr.status === 200 && xhr.readyState === 4) {
-			const famillesList = document.getElementById("familles");
-			const genresList = document.getElementById("genres");
-			const especesList = document.getElementById("especes");
+planteForm.addEventListener("input", getResultData);
 
-			const jsonObject = JSON.parse(xhr.responseText);
-			let genres = [];
-			let genres_id = [];
-			let especes = [];
-			let especes_id = [];
-			let familles = [];
-			let familles_id = [];
-			let fam = {};
-			let gen = {};
-			let esp = {};
-			let genreHtml = ``;
-			let especeHtml = ``;
-			let familleHtml = ``;
-			for (let i = 0; i < jsonObject.length; i++) {
-				let entry = jsonObject[i];
-				if (
-					!familles.includes(entry["famille"]) ||
-					!familles_id.includes(entry["famille_id"])
-				) {
-					familles.push(entry["famille"]);
-					familles_id.push(entry["famille_id"]);
-					fam[entry["famille_id"]] = entry["famille"];
-					familleHtml += `<li>${entry["famille"]}</li>`;
-				}
-				if (
-					!genres.includes(entry["genre"]) ||
-					!genres_id.includes(entry["genre_id"])
-				) {
-					genres.push(entry["genre"]);
-					genres_id.push(entry["genre_id"]);
-					gen[entry["genre_id"]] = entry["genre"];
-					genreHtml += `<li>${entry["genre"]}</li>`;
-				}
-				if (
-					!especes.includes(entry["espece"]) ||
-					!especes_id.includes(entry["espece_id"])
-				) {
-					especes.push(entry["espece"]);
-					especes_id.push(entry["espece_id"]);
-					esp[entry["espece_id"]] = entry["espece"];
-					especeHtml += `<li>${entry["espece"]}</li>`;
-				}
-			}
-			// console.log(fam, gen, esp);
-			famillesList.innerHTML = familleHtml;
-			genresList.innerHTML = genreHtml;
-			especesList.innerHTML = especeHtml;
-			// console.log("Genres ~~>", genres);
-			// console.log("Genres IDs ~~>", genres_id);
-			// console.log("Espèces ~~>", especes);
-			// console.log("Espèces IDs ~~>", especes_id);
-			// console.log("Familles ~~>", familles);
-			// console.log("Familles IDs ~~>", familles_id);
-		} else if (xhr.readyState === 4) {
-			console.log("Error occured");
-		}
-	};
-	xhr.send(send);
-	onFormChangeHideResults("familles", "famille");
-	onFormChangeHideResults("genres", "genre");
-	onFormChangeHideResults("especes", "espece");
-});
-
-function onFormChangeHideResults(table, column) {
-	if (document.getElementById(table).innerHTML == "") {
+function getResultData() {
+	if (document.getElementById("search").value != "") {
 		document
-			.getElementById(column)
+			.getElementById("results-grid")
 			.classList.remove("search-result-list-hidden");
-	} else {
+		const send = new FormData(planteForm);
+		const xhr = new XMLHttpRequest();
+		xhr.open("POST", "/pages/plantes/php/search_plantes.php", true);
+		xhr.onload = () => {
+			if (xhr.status === 200 && xhr.readyState === 4) {
+				const jsonObject = JSON.parse(xhr.responseText);
+				const familles = [];
+				const genres = [];
+				const especes = [];
+				html = `
+          <p class="result-header">Familles</p>
+          <p class="result-header">Genres</p>
+          <p class="result-header">Espèces</p>
+          `;
+				let counter = 0;
+				jsonObject.forEach((element) => {
+					if (!familles.includes(element.famille)) {
+						familles.push(element.famille);
+						html += `<p class="result" style="grid-row: ${
+							counter + 2
+						}; grid-column: 1;">${element.famille}</p>`;
+					}
+					if (!genres.includes(element.genre)) {
+						genres.push(element.genre);
+						html += `<p class="result" style="grid-row: ${
+							counter + 2
+						}; grid-column: 2;">${element.genre}</p>`;
+					}
+					if (!especes.includes(element.espece)) {
+						especes.push(element.espece);
+						html += `<p class="result result-espece" style="grid-row: ${
+							counter + 2
+						}; grid-column: 3;">${element.espece}</p>`;
+					}
+					counter++;
+				});
+				document.getElementById("results-grid").innerHTML = html;
+				console.log(familles);
+				console.log(genres);
+				console.log(especes);
+			}
+		};
+		xhr.send(send);
+	} else if (document.getElementById("search").value == "") {
 		document
-			.getElementById("famille")
+			.getElementById("results-grid")
 			.classList.add("search-result-list-hidden");
 	}
-}
-
-function clearOnEmptyForm(column) {}
-if (document.getElementById("search").value == "" || null || undefined) {
-	document.getElementById("familles").innerHTML = "";
 }
--- php/search_plantes.php
+++ php/search_plantes.php
@@ -1,25 +1,23 @@
 <?php
 
-if ($_POST["search"] != "") {
 $post = preg_replace("/'/", "\\'", $_POST["search"]);
-$post = preg_replace('/"/', "", $post); // to avoid SQL injections
+$post = preg_replace('/"/', "", $post);
 $post = preg_replace('/;/', "", $post); // to avoid SQL injections
 
 $mysqli = new mysqli("localhost", "root", "root", 'plantes');
-$result = $mysqli->query("SELECT familles.name AS famille, genres.name AS genre, especes.name AS espece, familles.id AS famille_id, genres.id AS genre_id, especes.id AS espece_id
+$result = $mysqli->query("SELECT DISTINCT familles.name AS famille, genres.name AS genre, especes.name AS espece
                           FROM especes
                           INNER JOIN familles ON especes.famille_id = familles.id
                           INNER JOIN genres ON especes.genre_id = genres.id
                           WHERE familles.name LIKE \"$post%\"
                           OR genres.name LIKE \"$post%\"
-                          OR especes.name LIKE \"$post%\";");
+                          OR especes.name LIKE \"$post%\"
+                          ORDER BY espece;");
 
 $arr = [];
 foreach ($result as $row) {
-   $arr[] = $row;
+  $arr[] = $row;
 }
 
 $json = json_encode($arr, JSON_UNESCAPED_UNICODE, 2);
 echo $json;
-
-}
